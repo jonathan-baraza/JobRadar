bf0163501acd0b4772c8fbf0e314985aa2dcc7bc
jonathan_baraza <jbaraza46@gmail.com>
2023-05-10 17:34:24 +0300

    about job details

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text } from "react-native";
+import style from "./about.style";
 
-const About = () => {
+const About = ({ info }) => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={style.container}>
+      <Text style={style.headText}>About the job</Text>
+      <View style={style.contentBox}>
+        <Text style={style.contextText}>{info}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
