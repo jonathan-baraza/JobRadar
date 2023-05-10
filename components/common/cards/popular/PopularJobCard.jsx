4c49448a4d993003188983835281b252d2bb6b9c
jonathan_baraza <jbaraza46@gmail.com>
2023-05-10 10:30:38 +0300

    popular jobs card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,7 +1,6 @@
 import { View } from "react-native";
 
 import styles from "./popularjobcard.style";
-import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return <View>PopularJobCard</View>;
